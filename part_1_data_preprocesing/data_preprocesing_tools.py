54deb1d7321b9fab3513e8a2b9f5de45739d7008
nemanja2799 <nemanjaivanovic2799@gmail.com>
2025-06-21 13:33:19 +0200

    Transformacija kolona sa tekstualnim sadrzajem u numericke kategorije pogodne za obradu

--- part_1_data_preprocesing/data_preprocesing_tools.py
+++ part_1_data_preprocesing/data_preprocesing_tools.py
@@ -24,9 +24,30 @@ imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
 imputer.fit(x[:, 1:3]) # these calculates for data range we set here
 x[:, 1:3] = imputer.transform(x[:, 1:3]) # these transform data for range we set here
 
+print(x)
+print(y)
+
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
-# transform values from textual categories(for example 3 languages: Spanish, German,Italian)
+# OneHotEncoding
+# transform values from textual categories(for example 3 languages: Spanish, German,Italian) in three categories with equal priority
 # in values(1,2,4(001, 010, 100) to make it easier for machine learning model to process them)
+
+# kind of transform way of transform and Clas of way of transformation,and thrid argument in transform is 
+# index od column we want to transform, reminder-way we want to treat other column(we keep them)
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+# model expect array in input
+
+x = np.array(ct.fit_transform(x))
+print(x)
+
+# for columns which containes values 'yes' or 'no' for some action like for example purchased we
+# encode just 1 for yes and 0 for no
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
+
+
