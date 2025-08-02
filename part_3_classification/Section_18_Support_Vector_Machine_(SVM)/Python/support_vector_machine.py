5127c035497e61bcabfa9413c33481bc05e4ebf8
nemanja2799 <nemanjaivanovic2799@gmail.com>
2025-08-02 15:18:24 +0200

    Uradjen primer za suport_vector_classification.

---
+++ part_3_classification/Section_18_Support_Vector_Machine_(SVM)/Python/support_vector_machine.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("part_3_classification/Section_18_Support_Vector_Machine_(SVM)/Python/Social_Network_Ads.csv")
+X = dataset.iloc[: , :-1].values
+y = dataset.iloc[: , -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25 , random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.svm import SVC
+
+classifier = SVC(kernel ="linear", random_state=0)
+classifier.fit(X_train, y_train)
+
+print(classifier.predict(sc.transform([[30, 87000]])))
+y_pred = classifier.predict(X_test)
+
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+print(confusion_matrix(y_test,y_pred))
+print(accuracy_score(y_test, y_pred))
